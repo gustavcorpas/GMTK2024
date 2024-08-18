5eb0e1fdfadcfa1f31df4345fd0b22f3ccbab820
Christian Christiansen <60815717+Jestic-Dev@users.noreply.github.com>
2024-08-18 22:45:58 +0200

    Recreated settings
    - Change organization
    - Still need to finalize functionality
    - added sliders

--- src/scenes/gameplay/settings.gd
+++ src/scenes/gameplay/settings.gd
@@ -11,6 +11,24 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_master_value_changed(value: float):
+#func _on_master_value_changed(value: float):
 	# AudioServer.set_bus_volume_db(0, value)
+	# AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_master_value_changed(value: float):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_music_value_changed(value: float):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+
+
+func _on_volume_sfx_value_changed(value: float):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound SFX"), value)
+
+
+
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/gameplay/Main_menu.tscn") 
+ 
