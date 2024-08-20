6992a703cf6b05ac15710135e312833383bb4a17
Christian Christiansen <60815717+Jestic-Dev@users.noreply.github.com>
2024-08-20 12:27:41 +0200

    Added functionally pause menu
    - Issues: after intereacting with resume it leave a white banner.

--- src/scenes/gameplay/pausemenu.gd
+++ src/scenes/gameplay/pausemenu.gd
@@ -1,34 +1,39 @@
 extends Control
 
+@onready var optionsMenu = preload("res://scenes/gameplay/Pausemenu.tscn")
+
 func _ready():
-	$AnimationPlayer.play("RESET")
+	$Animationpause.play("RESET")
 
 func resume():
-	get_tree().pause = false
-	$AnimationPlayer.play_backwards("Blur_screen")
+	get_tree().paused = false
+	$Animationpause.play_backwards("Blur_screen")
 	
 func pause():
-	get_tree().pause = true
-	$AnimationPlayer.play("Blur_screen")
+	get_tree().paused = true
+	$Animationpause.play("Blur_screen")
 
 func PressEcs():
-	if Input.is_action_just_pressed("Escape") and get_tree().paused == false:
+	if Input.is_action_just_pressed("Esc") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("Escape") and get_tree().paused == true:
+	elif Input.is_action_just_pressed("Esc") and get_tree().paused:
 		resume()
-
 
 func _on_resume_pressed():
 	resume()
-
 
 func _on_restart_pressed():
 	resume()
 	get_tree().reload_current_scene()
 
-
 func _on_quit_pressed():
-	get_tree().change_scene_to_file("res://assets/audio/menutheme.tscn")
+	get_tree().quit()
 
 func _process(delta):
 	PressEcs()
+
+func _on_options_pressed():
+	resume()
+	get_tree().change_scene_to_file("res://scenes/gameplay/Pausemenu.tscn")
+ 
+ 
