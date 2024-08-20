9de4872dea763b291a52ac9fdb3f5daf50b8e11c
Gustav Corpas <56586922+gustavcorpas@users.noreply.github.com>
2024-08-20 08:24:17 +0200

    glow and door goal thing
    create too much glow post pros lol

--- src/Goal.gd
+++ src/Goal.gd
@@ -1,8 +1,19 @@
 extends Area2D
 
 @export var scenePath :=  "res://scenes/gameplay/Level1.tscn"
+@onready var win: AudioStreamPlayer2D = $win
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var should_continue = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.name == "Player":
+		return
+	should_continue = true
+	animated_sprite_2d.play("open")
+	win.play()
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_win_finished() -> void:
+	if not should_continue: return
 	get_tree().change_scene_to_file(scenePath)
-	pass # Replace with function body.
